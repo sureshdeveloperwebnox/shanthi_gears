38875e5e7853473d0302497c7d692f6351cf8951
Swetha <swethabellan@gmail.com>
2025-09-22 13:09:38 +0530

    latest

--- src/app/api/complaints/route.js
+++ src/app/api/complaints/route.js
@@ -88,6 +88,7 @@ export async function GET() {
 export async function POST(req) {
   try {
     const body = await req.json();
+console.log(body);
 
     // Prepare data for WordPress
     const wordpressData = {
