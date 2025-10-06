655aea93d70281077594665104eaf382370607a4
Divesh2004 <110590441+Divesh2004@users.noreply.github.com>
2025-10-06 22:45:57 +0530

    Country_Crud

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -43,6 +43,12 @@ const UsersIcon = () => (
   </svg>
 );
 
+const CountriesIcon = () => (
+  <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
+    <path fillRule="evenodd" d="M10 2C5.58 2 2 5.58 2 10s3.58 8 8 8 8-3.58 8-8-3.58-8-8-8zM8 6a2 2 0 114 0 2 2 0 01-4 0zm0 4a2 2 0 114 0 2 2 0 01-4 0zm0 4a2 2 0 114 0 2 2 0 01-4 0z" clipRule="evenodd" />
+  </svg>
+);
+
 const GearIcon = () => (
   <svg className="w-4 h-4" fill="currentColor" viewBox="0 0 20 20">
     <path fillRule="evenodd" d="M11.49 3.17c-.38-1.56-2.6-1.56-2.98 0a1.532 1.532 0 01-2.286.948c-1.372-.836-2.942.734-2.106 2.106.54.886.061 2.042-.947 2.287-1.561.379-1.561 2.6 0 2.978a1.532 1.532 0 01.947 2.287c-.836 1.372.734 2.942 2.106 2.106a1.532 1.532 0 012.287.947c.379 1.561 2.6 1.561 2.978 0a1.533 1.533 0 012.287-.947c1.372.836 2.942-.734 2.106-2.106a1.533 1.533 0 01.947-2.287c1.561-.379 1.561-2.6 0-2.978a1.532 1.532 0 01-.947-2.287c.836-1.372-.734-2.942-2.106-2.106a1.532 1.532 0 01-2.287-.947zM10 13a3 3 0 100-6 3 3 0 000 6z" clipRule="evenodd" />
@@ -55,6 +61,7 @@ const links = [
   { href: "/territories", label: "Territories", icon: TerritoriesIcon },
   { href: "/employees", label: "Employees", icon: EmployeesIcon },
   { href: "/employee-territories", label: "Employee Territories", icon: EmployeeTerritoriesIcon },
+  { href: "/countries", label: "Countries", icon: CountriesIcon },
 ];
 
 const settings = [
